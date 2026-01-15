17bb94f7b5f20fcf417241b8db1fe07176509ccf
Yusuf Salk <yusuf.salk@huawei.com>
2026-01-15 10:03:44 +0300

    fix: robustly convert Windows paths to WSL format

--- src/transforms/windowsPathToWsl.ts
+++ src/transforms/windowsPathToWsl.ts
@@ -1,10 +1,9 @@
 import { join } from "path/posix";
-import { sep } from "path";
 
 export default (windowsPath: string) => {
     const driveLetter = windowsPath[0].toLowerCase();
     const posixyPath = windowsPath.replace(/^[^:]*:/, "") //remove drive letter
-        .split(sep).join("/"); //force / as separator
+        .replace(/\\/g, "/"); //force / as separator
         
     return join("/mnt/", driveLetter, posixyPath);
 }
